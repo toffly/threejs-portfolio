135d896c984863a44255dfad1423f3c4c00b0a12
toffly <toffly@outlook.com>
2026-03-22 12:46:15 +0700

    implements showcase section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Hero from "./components/sections/Hero"
+import ShowcaseSection from "./components/sections/ShowcaseSection"
 
 const App = () => {
   return (
     <>
       <Hero/>
+      <ShowcaseSection/>
     </>
   )
 }
---
+++ src/components/sections/ShowcaseSection.jsx
@@ -0,0 +1,92 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
+import { useRef } from "react";
+
+gsap.registerPlugin(ScrollTrigger);
+
+const ShowcaseSection = () => {
+  const sectionRef = useRef(null);
+  const project1Ref = useRef(null);
+  const project2Ref = useRef(null);
+  const project3Ref = useRef(null);
+
+  useGSAP(() => {
+    const projects = [
+      project1Ref.current,
+      project2Ref.current,
+      project3Ref.current,
+    ];
+    projects.forEach((card, index) => {
+      gsap.fromTo(
+        card,
+        {
+          y: 50,
+          opacity: 0,
+        },
+        {
+          y: 0,
+          opacity: 1,
+          duration: 1,
+          delay: 0.3 * (index + 1),
+          scrollTrigger: {
+            trigger: card,
+            start: "top bottom",
+          },
+        },
+      );
+    });
+
+    gsap.fromTo(
+      sectionRef.current,
+      { opacity: 0 },
+      {
+        opacity: 1,
+        duration: 1.5,
+      },
+    );
+  }, []);
+
+  return (
+    <section ref={sectionRef} id="work" className="app-showcase">
+      <div className="w-full">
+        <div className="showcaselayout">
+          <div ref={project1Ref} className="first-project-wrapper">
+            <div className="image-wrapper">
+              <img src="/images/project1.png" alt="Ryde" />
+            </div>
+            <div className="text-content">
+              <h2>Ryde - Ride Sharing App</h2>
+              <p className="text-white-50 md:text-xl">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Debitis quasi beatae adipisci nam, qui sed, minima alias
+                repellat aliquid tempora aperiam soluta architecto odio dicta
+                provident sequi doloribus sunt corporis.
+              </p>
+            </div>
+          </div>
+          <div
+            ref={project2Ref}
+            className="project-list-wrapper overflow-hidden"
+          >
+            <div className="project">
+              <div className="image-wrapper bg-[#ffefdb]">
+                <img src="/images/project2.png" alt="Project 2" />
+              </div>
+              <h2>Library Management Platform</h2>
+            </div>
+
+            <div ref={project3Ref} className="project">
+              <div className="image-wrapper bg-[#ffe7eb]">
+                <img src="/images/project3.png" alt="YC Directory" />
+              </div>
+              <h2>YC Directory - Startup Finder</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default ShowcaseSection;
